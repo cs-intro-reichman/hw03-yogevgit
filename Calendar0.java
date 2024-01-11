da3b8bb4d82cf892e8819d72d18bbe4b20775c1f
yogevgit <154701094+yogevgit@users.noreply.github.com>
2024-01-11 23:11:58 +0200

    Update Calendar0.java

--- Calendar0.java
+++ Calendar0.java
@@ -4,29 +4,36 @@
  */
 public class Calendar0 {	
 	
-	// Gets a year (command-line argument), and tests the functions isLeapYear and nDaysInMonth.
-	public static void main(String args[]) {
-		int year = Integer.parseInt(args[0]);
-		isLeapYearTest(year);
-		nDaysInMonthTest(year);
-	}
-		 
-	// Tests the isLeapYear function.
-	
-	private static void isLeapYearTest(int year) {
-		String commonOrLeap = "common";
-		if (isLeapYear(year)) {
-			commonOrLeap = "leap";
-		}
-		System.out.println(year + " is a " + commonOrLeap + " year");  
-	}
-
-	// Tests the nDaysInMonth function.
+	public static boolean isLeapYear(int year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            return year % 400 == 0;
+        } else {
+            return true;
+        }
+    }
+    return false;
+}
+	public static int nDaysInMonth(int month, int year) {
+    switch (month) {
+        case 2:
+            return isLeapYear(year) ? 29 : 28;
+        case 4: 
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+        default:
+            return 31;
+    }
+}
 	private static void nDaysInMonthTest(int year) {
-		for (int i = 1;i <= 12 ;i++ ) {
-			System.out.println("Month " + i + " has " + nDaysInMonth(i,year) + " days");
-			
-		}
+    for (int month = 1; month <= 12; month++) {
+        int days = nDaysInMonth(month, year);
+        System.out.println("Month " + month + " has " + days + " days");
+    }
+}
+	
 	}
 
 	
