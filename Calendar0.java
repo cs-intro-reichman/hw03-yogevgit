e4adaf0730c74445d30ddf7727859db9704bcd36
yogevgit <154701094+yogevgit@users.noreply.github.com>
2024-01-11 23:49:53 +0200

    Update Calendar0.java

--- Calendar0.java
+++ Calendar0.java
@@ -14,7 +14,7 @@ public class Calendar0 {
     }
     return false;
 }
-	public static int nDaysInMonth(int month, int year) {
+public static int nDaysInMonth(int month, int year) {
     switch (month) {
         case 2:
             return isLeapYear(year) ? 29 : 28;
@@ -27,60 +27,12 @@ public class Calendar0 {
             return 31;
     }
 }
-	private static void nDaysInMonthTest(int year) {
+private static void nDaysInMonthTest(int year) {
     for (int month = 1; month <= 12; month++) {
         int days = nDaysInMonth(month, year);
         System.out.println("Month " + month + " has " + days + " days");
     }
 }
-}	
-	
 
 	
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// Returns true if the given year is a leap year, false otherwise.
-	public static boolean isLeapYear(int year) {
-	    boolean isLeapYear;
-	    isLeapYear = ((year % 400) == 0);
-	    isLeapYear = isLeapYear || (((year % 4) == 0) && ((year % 100) != 0));
-
-		return isLeapYear;
-	}
-	 
 	
-	// Returns the number of days in the given month and year.
-	// April, June, September, and November have 30 days each.
-	// February has 28 days in a common year, and 29 days in a leap year.
-	// All the other months have 31 days.
-	public static int nDaysInMonth(int month, int year) {
-		if ((month == 4) || (month == 6) || (month == 9) || (month == 11) )
-			return 30;
-		else{
-			if ((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) || (month == 12))
-			{
-				return 31;
-			}
-			else {
-				if (isLeapYear(year))
-					return 29;
-				else
-					return 28;
-
-			}
-		}
-
-	}
-
-
