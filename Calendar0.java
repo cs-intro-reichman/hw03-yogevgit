a7510c8a70c756a0c8b9ee2db0763d36e72202eb
yogevgit10 <yogev.git@runi.ac.il>
2024-01-12 20:03:07 +0200

    Added a missing space

--- Calendar0.java
+++ Calendar0.java
@@ -2,70 +2,55 @@
  * Checks if a given year is a leap year or a common year,
  * and computes the number of days in a given month and a given year. 
  */
-public class Calendar0 {	
-	
-	// Gets a year (command-line argument), and tests the functions isLeapYear and nDaysInMonth.
+public class Calendar0 {
+
+	// Gets a year (command-line argument), and tests the functions isLeapYear and
+	// nDaysInMonth.
 	public static void main(String args[]) {
 		int year = Integer.parseInt(args[0]);
 		isLeapYearTest(year);
 		nDaysInMonthTest(year);
 	}
-		 
+
 	// Tests the isLeapYear function.
-	
+
 	private static void isLeapYearTest(int year) {
 		String commonOrLeap = "common";
 		if (isLeapYear(year)) {
 			commonOrLeap = "leap";
 		}
-		System.out.println(year + " is a " + commonOrLeap + " year");  
+		System.out.println(year + " is a " + commonOrLeap + " year");
 	}
 
 	// Tests the nDaysInMonth function.
 	private static void nDaysInMonthTest(int year) {
-		for (int i = 1;i <= 12 ;i++ ) {
-			System.out.println("Month " + i + " has" + nDaysInMonth(i,year) + " days");
-			
+		for (int i = 1; i <= 12; i++) {
+			System.out.println("Month " + i + " has " + nDaysInMonth(i, year) + " days");
+
 		}
 	}
 
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
 	// Returns true if the given year is a leap year, false otherwise.
 	public static boolean isLeapYear(int year) {
-	    boolean isLeapYear;
-	    isLeapYear = ((year % 400) == 0);
-	    isLeapYear = isLeapYear || (((year % 4) == 0) && ((year % 100) != 0));
+		boolean isLeapYear;
+		isLeapYear = ((year % 400) == 0);
+		isLeapYear = isLeapYear || (((year % 4) == 0) && ((year % 100) != 0));
 
 		return isLeapYear;
 	}
-	 
-	
+
 	// Returns the number of days in the given month and year.
 	// April, June, September, and November have 30 days each.
 	// February has 28 days in a common year, and 29 days in a leap year.
 	// All the other months have 31 days.
 	public static int nDaysInMonth(int month, int year) {
-		if ((month == 4) || (month == 6) || (month == 9) || (month == 11) )
+		if ((month == 4) || (month == 6) || (month == 9) || (month == 11))
 			return 30;
-		else{
-			if ((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) || (month == 12))
-			{
+		else {
+			if ((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10)
+					|| (month == 12)) {
 				return 31;
-			}
-			else {
+			} else {
 				if (isLeapYear(year))
 					return 29;
 				else
@@ -76,32 +61,3 @@ public class Calendar0 {
 
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
