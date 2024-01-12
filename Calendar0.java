f412d4dc55fb718343971ca5d8e60d7dc921d345
yogevgit10 <yogev.git@runi.ac.il>
2024-01-12 23:40:54 +0200

    upgrade

--- Calendar0.java
+++ Calendar0.java
@@ -48,7 +48,25 @@ public class Calendar0 {
 			return 29;
 		else if (isLeapYear(year) == false && month == 2)
 			return 28;
-		else if ((month == 4) || (month == 6) || (month == 9) && (month == 11))
+		else if ((month == 1))
+			return 31;
+		else if ((month == 3))
+			return 31;
+		else if ((month == 4))
+			return 30;
+		else if ((month == 5))
+			return 31;
+		else if ((month == 6))
+			return 30;
+		else if ((month == 7))
+			return 31;
+		else if ((month == 8))
+			return 31;
+		else if ((month == 9))
+			return 30;
+		else if ((month == 10))
+			return 31;
+		else if ((month == 11))
 			return 30;
 		else
 			return 31;
